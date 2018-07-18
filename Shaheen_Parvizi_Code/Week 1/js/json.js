71019fc5cdd9dbdfdae47b96b6dedda2d2addd42
Shaheen Parvizi <skippy7531@hotmail.com>
2018-07-18 14:49:20 -0500

    week 1 code

---
+++ Shaheen_Parvizi_Code/Week 1/js/json.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'Shaheen',
+    age: 24,
+    school: 'University of Houston'
+
+}
+
+let jsonStr = JSON.stringify(person); //Method to turn JS object into JSON
+
+
+let parsed = JSON.parse(jsonStr); //Method to turn JSON into JS object
+
+
+
+
