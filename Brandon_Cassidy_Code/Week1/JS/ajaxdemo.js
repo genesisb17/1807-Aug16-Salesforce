fd438e74ce27154c9b72243f9ee2ce9694729188
Brandon Cassidy <brandonleecassidy@gmail.com>
2018-07-19 14:39:35 -0400

    gitignore node + demo

--- Brandon_Cassidy_Code/Week1/JS/ajaxdemo.js
+++ Brandon_Cassidy_Code/Week1/JS/ajaxdemo.js
@@ -6,9 +6,8 @@ window.onload=function(){
 function getShip(){
     //get id from input
     var id=$('#shipID').val();
-    if(!id){
-        var searchName = $('searchName');
-    }//if
+    var searchName = $('#searchName').val();
+    console.log("getship " +id + " " + searchName);
     //Send ajax req, process response
 
     //1 create xhr object
@@ -19,19 +18,30 @@ function getShip(){
         console.log(Date() + " " + xhr.readyState);
         if(xhr.readyState==4 && xhr.status==200){
             //response processing code
+                if(id<2){
+
+                
+                var resp = xhr.responseText;
+                let ship = JSON.parse(resp);
+                $('#shipName').html("Starship: " + ship.results[0].name);
+                $('#shipCost').html("Cost in credits: " + ship.results[0].cost_in_credits);
+                }//if not id
+            else{
             var resp = xhr.responseText;
             let ship = JSON.parse(resp);
             $('#shipName').html("Starship: " + ship.name);
             $('#shipCost').html("Cost in credits: " + ship.cost_in_credits);
-        }//if
+            }//else
+            }//if ready and ok
     }//function/readystatechange
     //3 open request
-    if(id>0){
+    if(id>2){
     var url=`https://www.swapi.co/api/starships/${id}/`;
     }//if
-    if(searchName){
-        var url=`https://www.swapi.co/api/starships/?search=${searchName}/`;
+    if(id<2){
+        var url=`https://www.swapi.co/api/starships/?search=${searchName}`;
     }//if
+    console.log(url, searchName);
     xhr.open("GET",url,true);
 
     //4 send request, has requestbody param
