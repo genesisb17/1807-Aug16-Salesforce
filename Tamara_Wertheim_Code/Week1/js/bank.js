70ee557adcafc2330a8c9e139db5d85bc90f7cb6
TMWertheim <TamaraMWertheim@gmail.com>
2018-07-19 21:13:14 -0400

    Updating files

--- Tamara_Wertheim_Code/Week1/js/bank.js
+++ Tamara_Wertheim_Code/Week1/js/bank.js
@@ -1,36 +1,48 @@
 window.onload = function(){
-    $('#register').on('click', register);
     $('#login').on('click', login);
+    $('#register').on('click', registerView);
 }
 
 function login(){
-    let url = `http://localhost:3000/users?username=${$('#username').val}`;
-    let xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status==200){
-            let arr = JSON.parse(xhr.responseText);
-            if(arr.length == 0){
-                console.log("You don't exist!!!")
-            } else {
-                let user = arr[0];
-                if (user.password == $(`#pw`).val()){
-                    console.log("Correct")
-                } else {
-                    console.log("NOPE!");
-                }
-            }
-        }
-    }
-        xhr.open("GET", "http://localhost:3000/users", true);
-        xhr.setRequestHeader("Content-type", "application/json")
-        xhr.send(JSON.stringify(url));    
+ let url = `http://localhost:3000/users?username=${$('#username').val()}`;
+ let xhr = new XMLHttpRequest();
+ xhr.onreadystatechange = function(){
+     if(xhr.readyState==4 && xhr.status==200){
+         var arr = JSON.parse(xhr.responseText);
+         if(arr.length==0){
+             //username does not exist
+             $('#err').html("Username NonExistent. Try again");
+             $('#err').attr("hidden", false);
+             console.log("username nonexistent")
+         }
+         else{
+             //username does exist. now check to see if pw matches
+             let user = arr[0];
+             if(user.password == $('#pw').val()){
+                $('#err').attr("hidden", true);
+                $('#success').attr("hidden", false);
+                $('#success').html("Success! You are logged in");
+                 //credentials are correct
+                 console.log("Correct credentials");
+             }
+             else{
+                $('#err').html("Incorrect Password. Try again");
+                $('#err').attr("hidden", false);
+                 console.log("incorrect credentials");
+             }
+         }
+     }
+ }
+ xhr.open("GET", url, true);
+ xhr.send();
+
 }
 
 function registerView(){
-    $(`#registerView`).attr('hidden',false);
-    $(`#register`).off('click');
-    $(`#register`).on('click', register);
+    $('#registerView').attr('hidden', false);
+    $('#register').off('click');
+    $('#register').on('click', register );
 }
 
 function register(){
@@ -41,16 +53,17 @@ function register(){
         password: $('#pw').val(),
         balance: 0.00
     }
-
-    var req = new XMLHttpRequest();
-
+    //AJAX! 
+    let req = new XMLHttpRequest();
     req.onreadystatechange = function(){
-        console.log(Date() + " " + req.readyState);
-        if(req.readyState == 4 && req.status==201){
+        console.log(req.status);
+        if(req.status==201 && req.readyState==4){
             console.log(req.responseText);
         }
     }
-        req.open("POST", "http://localhost:3000/users", true);
-        req.setRequestHeader("Content-type", "application/json")
-        req.send(JSON.stringify(user));    
+    req.open("POST", "http://localhost:3000/users", true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify(user));
+
+
 }
