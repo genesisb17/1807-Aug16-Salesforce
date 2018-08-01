1c6b0ab2695e36759e535f593496077d8dfb519d
Matthew Smart <msmart1994@gmail.com>
2018-08-01 10:05:34 -0400

    Final Commit for Presentation

--- Matt_Smart_Code/Project 0/pokemon.js
+++ Matt_Smart_Code/Project 0/pokemon.js
@@ -12,6 +12,8 @@ window.onload = function() {
     numBattles = 0;
     mull=false;
 }
+var activeAjaxConnections = 0;
+var stage = 0;
 var battleState = {
 playerTeam: [],
 enemyTeam: [],
@@ -48,6 +50,7 @@ function getPokemonTeam() {
     //callback functions
     xhr1.onreadystatechange = function (){
         if(xhr1.readyState == 4 && xhr1.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr1.responseText;
             let pokemon = JSON.parse(resp);
@@ -57,6 +60,7 @@ function getPokemonTeam() {
     };
     xhr2.onreadystatechange = function (){
         if(xhr2.readyState == 4 && xhr2.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr2.responseText;
             let pokemon = JSON.parse(resp);
@@ -66,6 +70,7 @@ function getPokemonTeam() {
     };
     xhr3.onreadystatechange = function (){
         if(xhr3.readyState == 4 && xhr3.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr3.responseText;
             let pokemon = JSON.parse(resp);
@@ -75,6 +80,7 @@ function getPokemonTeam() {
     };
     xhr4.onreadystatechange = function (){
         if(xhr4.readyState == 4 && xhr4.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr4.responseText;
             let pokemon = JSON.parse(resp);
@@ -84,6 +90,7 @@ function getPokemonTeam() {
     };
     xhr5.onreadystatechange = function (){
         if(xhr5.readyState == 4 && xhr5.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr5.responseText;
             let pokemon = JSON.parse(resp);
@@ -93,6 +100,7 @@ function getPokemonTeam() {
     };
     xhr6.onreadystatechange = function (){
         if(xhr6.readyState == 4 && xhr6.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr6.responseText;
             let pokemon = JSON.parse(resp);
@@ -109,7 +117,7 @@ function getPokemonTeam() {
     url = `https://pokeapi.co/api/v2/pokemon/${ids[4]}/`; xhr5.open("GET", url, true);
     url = `https://pokeapi.co/api/v2/pokemon/${ids[5]}/`; xhr6.open("GET", url, true);
     progBar.value = 10;
-
+    activeAjaxConnections = 6;
     //send
     xhr1.send();
     xhr2.send();
@@ -118,6 +126,22 @@ function getPokemonTeam() {
     xhr5.send();
     xhr6.send();
     progBar.value = 25;
+
+}
+//function for enabling screen functionality once Ajax requests are done.
+function enable(i) {
+    if(i == 0) {
+        progBar.value = 100;
+        $(`.sprite`).on('click', clearSelect);
+        $(`.sprite`).on('click', dispPokemon);    
+        $(`.move-btn`).on('click', dispMove);
+        $(`#Name`).html("Select a Pokemon to view information");
+    }
+    if(i == 1) {
+        //$(`#loadScreen`).attr("hidden", true);
+        clearInterval();
+       // $(`#storeScreen`).removeAttr("hidden");
+    }
 }
 /*
 Separate function for generating enemy teams
@@ -131,6 +155,7 @@ function generateEnemyTeam() {
     let xhr4 = new XMLHttpRequest();
     let xhr5 = new XMLHttpRequest();
     let xhr6 = new XMLHttpRequest();
+    activeAjaxConnections = 6;
     //Define callback functions, generates a moveset for each
     var ids = [Math.floor(Math.random() * 712) + 1,
         Math.floor(Math.random() * 712) + 1,
@@ -143,56 +168,68 @@ function generateEnemyTeam() {
     //callback functions
     xhr1.onreadystatechange = function (){
         if(xhr1.readyState == 4 && xhr1.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr1.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[0] = pokemon;
             determineMoveset(enemyTeam[0], 1);
+            displayEnemy(enemyTeam[0], 1);
         }
     };
     xhr2.onreadystatechange = function (){
         if(xhr2.readyState == 4 && xhr2.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr2.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[1] = pokemon;
             determineMoveset(enemyTeam[1], 2);
+            displayEnemy(enemyTeam[1], 2);
         }
     };
     xhr3.onreadystatechange = function (){
         if(xhr3.readyState == 4 && xhr3.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr3.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[2] = pokemon;
             determineMoveset(enemyTeam[2], 3);
+            displayEnemy(enemyTeam[2], 3);
         }
     };
     xhr4.onreadystatechange = function (){
         if(xhr4.readyState == 4 && xhr4.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr4.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[3] = pokemon;
             determineMoveset(enemyTeam[3], 4);
+            displayEnemy(enemyTeam[3], 4);
         }
     };
     xhr5.onreadystatechange = function (){
         if(xhr5.readyState == 4 && xhr5.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr5.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[4] = pokemon;
             determineMoveset(enemyTeam[4], 5);
+            displayEnemy(enemyTeam[4], 5);
         }
     };
     xhr6.onreadystatechange = function (){
         if(xhr6.readyState == 4 && xhr6.status == 200) {
+            activeAjaxConnections--;
             //process response
             let resp = xhr6.responseText;
             let pokemon = JSON.parse(resp);
             enemyTeam[5] = pokemon;
             determineMoveset(enemyTeam[5], 6);
+            displayEnemy(enemyTeam[5], 6);
         }
     };
 
@@ -202,7 +239,7 @@ function generateEnemyTeam() {
     url = `https://pokeapi.co/api/v2/pokemon/${ids[2]}/`; xhr3.open("GET", url, true);
     url = `https://pokeapi.co/api/v2/pokemon/${ids[3]}/`; xhr4.open("GET", url, true);
     url = `https://pokeapi.co/api/v2/pokemon/${ids[4]}/`; xhr5.open("GET", url, true);
-    url = `https://pokeapi.co/api/v2/pokemon/${ids[5]}/`; xhr6.open("GET", url, false);
+    url = `https://pokeapi.co/api/v2/pokemon/${ids[5]}/`; xhr6.open("GET", url, true);
 
     //send
     xhr1.send();
@@ -211,21 +248,33 @@ function generateEnemyTeam() {
     xhr4.send();
     xhr5.send();
     xhr6.send();
+
 }
 function setPokemonValues(pokemon, i) {
-
     //Party Display
     let image= $(`#sprite${i}`);
-    image.attr("src", pokemon.sprites.front_default);
+    let shiny = Math.floor(Math.random() * 1000) + 1;
+    if(shiny % 15 == 0) {
+        image.attr("src", pokemon.sprites.front_shiny);
+    }
+    else {
+        image.attr("src", pokemon.sprites.front_default);
+    }
     image.attr("alt", pokemon.name);
 
     //Determine Ability
+    activeAjaxConnections++;
     let xhrAbility = new XMLHttpRequest();
     xhrAbility.onreadystatechange = function (){
         if(xhrAbility.readyState == 4 && xhrAbility.status==200){
+            activeAjaxConnections--;
             let resp = xhrAbility.responseText;
             let ability = JSON.parse(resp);
             pokemon.abilities = [ability.name, ability.effect_entries[0].effect];
+            if(activeAjaxConnections==0) {
+                enable(stage);
+                stage++;
+            }
         }
     }
     let numAbilities = pokemon.abilities.length;
@@ -237,16 +286,44 @@ function setPokemonValues(pokemon, i) {
     for(i=0; i < 4; i++) {
         determineMoveset(pokemon, i);
     }
+    determineRarity(pokemon);
 };
+//Determine Rarity
+function determineRarity(pokemon) {
+    let xhrRare = new XMLHttpRequest();
+    activeAjaxConnections++;
+    xhrRare.onreadystatechange = function (){
+        if(xhrRare.readyState == 4 && xhrRare.status == 200){
+            activeAjaxConnections--;
+            let resp = xhrRare.responseText;
+            let rare = JSON.parse(resp);
+            pokemon.rarity = rare.capture_rate;
+            if(activeAjaxConnections==0) {
+                enable(stage);
+                stage++;
+            }
+        }
+    }
+    var url = `https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}/`;
+    xhrRare.open("GET", url, true);
+
+    //send XHTTP request
+    xhrRare.send();
+}
 //Determine Moveset
 function determineMoveset(pokemon, slotNum) {
     let xhrMoves = new XMLHttpRequest();
-
+    activeAjaxConnections++;
     xhrMoves.onreadystatechange = function (){
         if(xhrMoves.readyState == 4 && xhrMoves.status == 200){
+            activeAjaxConnections--;
             let resp = xhrMoves.responseText;
             let move = JSON.parse(resp);
             pokemon.moves[slotNum] = move;
+            if(activeAjaxConnections==0) {
+                enable(stage);
+                stage++;
+            }
         }
     }
 
@@ -255,24 +332,22 @@ function determineMoveset(pokemon, slotNum) {
     let movesNum = Math.floor(Math.random() * numMoves) + 0;
     var url = `https://pokeapi.co/api/v2/move/${pokemon.moves[movesNum].move.name}/`;
     xhrMoves.open("GET", url, true);
-    //enable page functionality once Pokemon are loaded (close enough)
-    xhrMoves.onloadend = function(e) {
-        if(xhrMoves.readyState == 4 && xhrMoves.status == 200) {
-            progBar.value = 100;
-            $(`.sprite`).on('click', clearSelect);
-            $(`.sprite`).on('click', dispPokemon);    
-            $(`.move-btn`).on('click', dispMove);
-        }
-    };
+
     //send XHTTP request
     xhrMoves.send();
 }
-
+function displayEnemy(pokemon, i) {
+    let image= $(`#esprite${i}`);
+    image.attr("src", pokemon.sprites.front_default);
+    image.attr("alt", pokemon.name);
+}
 
 
 function dispPokemon() {
     //Add selection display
     addSelect(this);
+    $(`.fa`).removeClass("checked");
+    $(`.fa`).removeAttr("hidden");
 
     //Hide progress bar
     progressBar.setAttribute("hidden", true);
@@ -286,6 +361,22 @@ function dispPokemon() {
     //Set Focus
     let image= $(`#focus`);
     curr_pokemon = pokemonTeam[pokeNum];
+    let rare = curr_pokemon.rarity;
+    if(rare < 250) {
+        $(`#star1`).addClass("checked");
+    }
+    if(rare < 200) {
+        $(`#star2`).addClass("checked");
+    }
+    if(rare < 120) {
+        $(`#star3`).addClass("checked");
+    }
+    if(rare < 60) {
+        $(`#star4`).addClass("checked");
+    }
+    if(rare < 20) {
+        $(`#star5`).addClass("checked");
+    }
     image.attr("src", curr_pokemon.sprites.front_default);
     image.attr("alt", curr_pokemon.name);
     $(`#focus`).removeClass(`pokeNum-0 pokeNum-1 pokeNum-2 pokeNum-3 pokeNum-4 pokeNum-5`);
@@ -297,14 +388,14 @@ function dispPokemon() {
         if(i == 1) {
             $(`#Type1`).removeClass("normal fighting flying poison ground rock bug ghost steel fire water grass electric psychic ice dragon dark fairy");
             $(`#Type1`).addClass(`${element.type.name}`);
-            $(`#Type1`).html(`${element.type.name}`[0].toUpperCase()+`${element.type.name}`.substring(1,`${element.type.name}`.length));
+            $(`#Type1`).html(`${element.type.name}`.toUpperCase());
             $(`#Type2`).html('');
         }
 
         if(i == 2) {
             $(`#Type2`).removeClass("normal fighting flying poison ground rock bug ghost steel fire water grass electric psychic ice dragon dark fairy");
             $(`#Type2`).addClass(`${element.type.name}`);
-            $(`#Type2`).html(`${element.type.name}`[0].toUpperCase()+`${element.type.name}`.substring(1,`${element.type.name}`.length));
+            $(`#Type2`).html(`${element.type.name}`.toUpperCase());
         }
         i++;
     });
@@ -338,7 +429,7 @@ function dispPokemon() {
 
     function displayStats(){
         for(var j=0; j<curr_pokemon.stats.length; j++) {
-            $(`#${curr_pokemon.stats[j].stat.name}`).html(`${curr_pokemon.stats[j].stat.name}: ${curr_pokemon.stats[j].base_stat}`);
+            $(`#${curr_pokemon.stats[j].stat.name}`).html(`${curr_pokemon.stats[j].stat.name.toUpperCase()}: ${curr_pokemon.stats[j].base_stat}`);
         }
     }
     
@@ -356,7 +447,7 @@ function dispMove() {
     let currChild = moveDisp.childNodes[0];
 
     $(`#movePanel`).addClass(`${curr_pokemon.moves[moveNum].type.name}`);
-    powerpp.innerHTML = `${curr_pokemon.moves[moveNum].type.name} / `+ `${curr_pokemon.moves[moveNum].damage_class.name} / ` + `${curr_pokemon.moves[moveNum].power}` + " Power / "
+    powerpp.innerHTML = `${curr_pokemon.moves[moveNum].type.name.toUpperCase()} / `+ `${curr_pokemon.moves[moveNum].damage_class.name} / ` + `${curr_pokemon.moves[moveNum].power}` + " Power / "
     + `${curr_pokemon.moves[moveNum].pp}` + " PP";
     flavor.innerHTML = `${curr_pokemon.moves[moveNum].flavor_text_entries[2].flavor_text}`;
 
@@ -379,7 +470,7 @@ function mulligan() {
     this.setAttribute("hidden", true);
     xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function (){
-        if(xhr.readyState == 4 && xhr1.status == 200) {
+        if(xhr.readyState == 4 && xhr.status == 200) {
             //process response
             let resp = xhr.responseText;
             let pokemon = JSON.parse(resp);
@@ -400,22 +491,67 @@ function clearSelect() {
    for(i=0; i < imgs.length; i++) {
        imgs[i].style.border = "";
    }
+   $(`#movePanel`).attr("hidden", true);
 }
 function addSelect(tag) {
     tag.setAttribute("style", "border:2px solid rgb(233, 171, 88)");
     $(`#movePanel`).attr("hidden");
 }
+function quickDisp() {
+    //Add selection display
+    addSelect(this);
+    var sprite = this.getAttribute("id");
+    var pokeNum;
+
+    if(/esprite/i.test(this.getAttribute("id"))) {
+        pokeNum = sprite.substring(7,8);
+        pokeNum = parseInt(pokeNum) - 1;
+        curr_pokemon = enemyTeam[pokeNum];
+    }
+    else {
+        pokeNum = sprite.substring(6,7);
+        pokeNum = parseInt(pokeNum) - 1;
+        curr_pokemon = pokemonTeam[pokeNum];
+    }
+    /*
+    let i=1;
+    curr_pokemon.types.forEach(element => {
+        if(i == 1) {
+            $(`#qType1`).removeClass("normal fighting flying poison ground rock bug ghost steel fire water grass electric psychic ice dragon dark fairy");
+            $(`#qType1`).addClass(`${element.type.name}`);
+            $(`#qType1`).html(`${element.type.name}`.toUpperCase());
+            $(`#qType2`).html('');
+        }
+
+        if(i == 2) {
+            $(`#qType2`).removeClass("normal fighting flying poison ground rock bug ghost steel fire water grass electric psychic ice dragon dark fairy");
+            $(`#qType2`).addClass(`${element.type.name}`);
+            $(`#qType2`).html(`${element.type.name}`.toUpperCase());
+        }
+        i++;
+    });
+    */
+
+}
 
 //Script for starting a battle
 function startBattle() {
-    //Set up 'loading screen'
-    $(`#teamInfo`).attr("hidden", true);
-    $(`#loadScreen`).removeAttr("hidden");
+    //Set up prebattle screen
+    $(`#info`).attr("hidden", true);
+    $(`#movePanel`).attr("hidden", true);
+    $(".sprite").prop("onclick", null).off("click");
+    $(`.sprite`).on('click', quickDisp);
+    $(`.sprite`).on('click', clearSelect);
+    $(`.move-btn`).attr("hidden", true);
+    $(`#enemies`).removeAttr("hidden");
+    $(`#spacer`).removeAttr("hidden");
+    $(`#findOpponent`).attr("hidden", true);
+    //For the presentation, there will be no battle
     //Create enemy trainer and their team
     generateEnemyTeam();
     //display Enemy Team on the Load Screen
-
     //Set up pre-Battle/Battle Screens
+
 
     //Enable functionality of battle elements
 
@@ -495,7 +631,7 @@ function useMove() {
         */
 
         //determine if there is a status ailment
-        if(move.effect)
+        if(move.effect);
     }
 }
 function changePokemon() {
