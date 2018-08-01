505bf59c516a4b70e4ac534072ca8ee1542d79ab
Brandon <brandonleecassidy@gmail.com>
2018-07-31 20:53:39 -0400

    Functioning project 0

--- Brandon_Cassidy_Code/Projects/Project 0.js
+++ Brandon_Cassidy_Code/Projects/Project 0.js
@@ -1,13 +1,21 @@
 const numStars = 200;
-const picURLs = {"Death Star" : "https://lumiere-a.akamaihd.net/v1/images/open-uri20150608-27674-1izkue_bc561fd4.jpeg?region=0%2C0%2C1200%2C675",
-                "CR90 Corvette" : "https://steamuserimages-a.akamaihd.net/ugc/385415243847790290/26703058FD9F4273238FF5CBDC4C8DB8D8B05849/?interpolation=lanczos-none&output-format=jpeg&output-quality=95&fit=inside%7C637%3A358&composite-to=*,*%7C637%3A358&background-color=black",
-                "Star Destroyer" : "https://cdn.shopify.com/s/files/1/0747/3829/products/mV0110_1024x1024.jpeg?v=1485014193",
-                "Sentinel-class landing craft" : "https://vignette.wikia.nocookie.net/starwars/images/5/5b/Imperial_Sentinel-class_shuttle.png/revision/latest?cb=20161008235432",
-                "Millennium Falcon" : "https://upload.wikimedia.org/wikipedia/en/thumb/8/8d/A_screenshot_from_Star_Wars_Episode_IV_A_New_Hope_depicting_the_Millennium_Falcon.jpg/250px-A_screenshot_from_Star_Wars_Episode_IV_A_New_Hope_depicting_the_Millennium_Falcon.jpg",
-                "Y-wing" : "https://lumiere-a.akamaihd.net/v1/images/Y-Wing-Fighter_0e78c9ae.jpeg?region=0%2C0%2C1536%2C864&width=768",
-                "" : ""};
+
+//Numbers for dict:
+//23,22,21,13,12,11,10,9,5,15
+// const dict={1:"5",
+//             2:"9",
+//             3:"10",
+//             4:"11",
+//             5:"12",
+//             6:"13",
+//             7:"21",
+//             8:"22",
+//             9:"23",
+//             10:"15"
+//             }
+
 window.onload = function(){
-    $('checkSafety').on('click',displaySafeState);
+  populateOptions();
     for (let i = 0; i < numStars; i++) {
         let star = document.createElement("div");  
         star.className = "star";
@@ -16,6 +24,7 @@ window.onload = function(){
         star.style.left = xy[1] + 'px';
         document.body.append(star);
       }//for
+      $('#getShip').on('click',getShip);
 }//onload
 // Gets random x, y values based on the size of the container
 function getRandomPosition() {  
@@ -25,7 +34,90 @@ function getRandomPosition() {
   var randomY = Math.floor(Math.random()*y);
   return [randomX,randomY];
 }
+function populateOptions(){
+    //1 create xhr object
+    console.log("popopts");
+    var xhr = new XMLHttpRequest();
 
-function displaySafeState(){
-    
-}
+    //2 callback function
+    xhr.onreadystatechange = function(){
+        console.log(Date() + " " + xhr.readyState);
+        if(xhr.readyState==4 && xhr.status==200){
+            //response processing code
+              var resp = xhr.responseText;
+              let ships = JSON.parse(resp);
+              let numShips=1;
+              for(var shipnum in ships.results){
+                let selectShip = document.createElement('option');
+                let ship = ships.results[shipnum];
+                selectShip.text=ship.name;
+                urlBreakdown = ship.url.split('/');
+                selectShip.value = urlBreakdown[5];
+                console.log(selectShip.value);
+                $('#searchName').append(selectShip);
+                numShips++;
+              }
+            }//if ready and ok
+    }//function/readystatechange
+  
+    //3 open request
+    var url=`https://www.swapi.co/api/starships`;
+    console.log(url);
+    xhr.open("GET",url,true);
+      //4 send request, has requestbody param
+    xhr.send();
+}//populateOptions
+
+function getShip(){
+  //get id from input
+  var id=$('#searchName').val();
+  console.log("getship " +id);
+  $('#showCrew').html('');
+  $('#showSize').html('');
+  $('#showCost').html('');
+  $('#showName').html('');
+  $('#content').removeClass("content");
+  //Send ajax req, process response
+
+  //1 create xhr object
+  var xhr = new XMLHttpRequest();
+
+  //2 callback function
+  xhr.onreadystatechange = function(){
+      console.log(Date() + " " + xhr.readyState);
+      if(xhr.readyState==4 && xhr.status==200){
+          //response processing code
+            var resp = xhr.responseText;
+            let ship = JSON.parse(resp);
+            // let shipName = document.createElement('output');
+            // let shipCost = document.createElement('output');
+            // let shipLength = document.createElement('output');
+            // let shipCrew = document.createElement('output');
+            $('#showCrew').html('Crew size:  ' +ship.crew);
+            $('#showSize').html('Length(in kuats):  ' +ship.length);
+            $('#showCost').html('Cost(in credits):  ' +ship.cost_in_credits);
+            $('#showName').html(ship.name);
+            // console.log(shipCrew);
+            // console.log(shipCost);
+            $('#content').addClass("content");
+            // console.log(ship.name + "ship name");
+            // console.log(ship.cost_in_credits + "ship cost");
+          }//if ready and ok
+  }//function/readystatechange
+
+  //3 open request
+  var url=`https://www.swapi.co/api/starships/${id}/`;
+  console.log(url);
+  xhr.open("GET",url,true);
+
+  //4 send request, has requestbody param
+  xhr.send();
+
+  //following this, execution continues as if request never happened; non-waiting
+  //response does not hold up remainder of program
+  console.log("CODE AFTER REQUEST WAS SENT");
+
+
+}//getShip
+
+
